685ba075797ea385c623684471d31c3f6a4f8e56
Tomáš Silber <silber.tomas@outlook.cz>
2021-11-14 18:36:19 +0100

    Do not allow edit large text, support random and null vector

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import *
 import tkinter as tk
 import pygubu
 import base64
+import os
 
 class MainWindow:
     def __init__(self):
@@ -18,6 +19,7 @@ class MainWindow:
         self.text_load_btn = builder.get_object('text_load_btn')
         self.input_text = builder.get_object('input_text')
         self.key_field = builder.get_object('key_field')
+        self.errorLabel = builder.get_object('errorLabel')
 
         self.vector_group = builder.get_variable("vector_group")
         self.mode_group = builder.get_variable("mode_group")
@@ -35,55 +37,76 @@ class MainWindow:
         self.radiobutton6 = builder.get_object('radiobutton6')
         self.radiobutton7 = builder.get_object('radiobutton7')
         self.key_callback()
+        self.big_input = False
 
         self.file_path = ""
 
     def encrypt(self, message, key, mode):
-        #Vygenerování náhodného vektoru
-        iv = Random.new().read(AES.block_size)
-        #Použít iv v knihovně v případě CBC
-        if mode == AES.MODE_CBC:
-            cipher = AES.new(key, mode, iv)
-        else:
-            cipher = AES.new(key, mode)
+        try:
+            if self.vector == 1:
+                # Vygenerování náhodného vektoru
+                iv = Random.new().read(AES.block_size)
+            else:
+                #Vygenerování vektoru dosazením samých nul
+                iv = b'\0' * AES.block_size
+            # Použít iv v případě CBC
+            if mode == AES.MODE_CBC:
+                cipher = AES.new(key, mode, iv)
+            else:
+                cipher = AES.new(key, mode)
 
-        #Uspořádaní dat po blocích
-        padding = AES.block_size - len(message) % AES.block_size
-        message += bytes([padding]) * padding
+            # Uspořádaní dat po blocích
+            padding = AES.block_size - len(message) % AES.block_size
+            message += bytes([padding]) * padding
 
-        #Zašifrování textu
-        data = iv + cipher.encrypt(message)
-        #Zakodování do BASE64
-        result = base64.b64encode(data)
+            # Zašifrování textu
+            data = iv + cipher.encrypt(message)
+            # Zakodování do BASE64
+            result = base64.b64encode(data)
 
-        #Nastavení textu v GUI
-        self.input_text.delete(1.0, "end")
-        self.input_text.insert(1.0, result)
+            #Z výkonostních důvodů zakázat editaci textu v případě velkého vstupu
+            if(self.big_input):
+                self.input_text.config(state=NORMAL)
+
+            #Nastavení textu v GUI
+            self.input_text.delete(1.0, "end")
+            self.input_text.insert(1.0, result)
+
+            if (self.big_input):
+                self.input_text.config(state=DISABLED)
+        except Exception as e:
+            raise Exception("Něco se nepovedlo. Zkuste překontrolovat konfiguraci.")
 
     def decrypt(self, message, key, mode):
-        #Dekodování zprávy BASE64
-        message = base64.b64decode(message)
-        #Získání vektoru (první blok -> prvních 16 B)
-        iv = message[:AES.block_size]
+        try:
+            # Dekodování zprávy BASE64
+            message = base64.b64decode(message)
+            # Získání vektoru (první blok -> prvních 16 B)
+            print(AES.block_size)
+            iv = message[:AES.block_size]
 
-        #Použít iv v knihovně v případě CBC
-        if mode == AES.MODE_CBC:
-            cipher = AES.new(key, mode, iv)
-        else:
-            cipher = AES.new(key, mode)
-        #Dešifrování dat
-        data = cipher.decrypt(message[AES.block_size:])
-        padding = data[-1]
+            # Použít iv v případě CBC
+            if mode == AES.MODE_CBC:
+                cipher = AES.new(key, mode, iv)
+            else:
+                cipher = AES.new(key, mode)
+            # Dešifrování dat
+            data = cipher.decrypt(message[AES.block_size:])
+            padding = data[-1]
 
-        if data[-padding:] != bytes([padding]) * padding:
-            raise ValueError("Invalid padding...")
+            if data[-padding:] != bytes([padding]) * padding:
+                raise ValueError("Invalid padding...")
 
-        self.input_text.delete(1.0, "end")
-        self.input_text.insert(1.0, data[:-padding].decode("utf-8"))
+            self.input_text.delete(1.0, "end")
+            self.input_text.insert(1.0, data[:-padding].decode("utf-8"))
+        except Exception as e:
+            raise Exception("Něco se nepovedlo. Zkuste překontrolovat konfiguraci.")
 
     def encrypt_btn_click(self):
+        self.input_text.config(state=NORMAL)
         #Získání zprávy k zakódování a zakódování textu do UTF-8
         message = self.input_text.get("1.0", END).encode('utf-8')
+
         key = self.key_field.get()
         if len(key) < self.key_len or len(key) > self.key_len:
             raise Exception("Délka klíče musí být: {} znaků. Máte {} znaků".format(self.key_len, len(key)))
@@ -93,6 +116,7 @@ class MainWindow:
         self.encrypt(message, key, self.mode)
 
     def decrypt_btn_click(self):
+        self.input_text.config(state=NORMAL)
         # Získání zprávy k zakódování a zakódování textu do UTF-8
         message = self.input_text.get("1.0", END).encode('utf-8')
         key = self.key_field.get()
@@ -105,32 +129,49 @@ class MainWindow:
         self.decrypt(message, key, self.mode)
 
     def text_load_btn_click(self):
-        file_path = filedialog.askopenfilename(title="Vyber textový soubor", filetypes=
-        [
-            ('text files', ('.txt'))
-        ])
-        if not file_path or file_path is None or file_path == "":
-            return
+        try:
+            file_path = filedialog.askopenfilename(title="Vyber textový soubor", filetypes=
+            [
+                ('text files', ('.txt'))
+            ])
+            if not file_path or file_path is None or file_path == "":
+                return
 
-        with open(file_path, 'r') as file:
-            data = file.read()
-            file.close()
+            with open(file_path, 'r') as file:
+                data = file.read()
+                file.close()
 
-        self.input_text.delete(1.0, "end")
-        self.input_text.insert(1.0, data)
+            file_size = os.path.getsize(file_path)
+
+            self.big_input = True if file_size >= 1000000 else False
+
+            self.input_text.delete(1.0, "end")
+            self.input_text.insert(1.0, data)
+
+            if self.big_input:
+                self.input_text.config(state=DISABLED)
+                self.errorLabel.config(text='Vstup příliš velký, editace textu vypnuta.')
+            else:
+                self.input_text.config(state=NORMAL)
+                self.errorLabel.config(text='')
+        except Exception as e:
+            raise Exception("Nepovedlo se načíst soubor: " + str(e))
 
     def save_btn_click(self):
-        save_location = filedialog.asksaveasfile(mode='w', initialfile="cipher", defaultextension=".txt",
-                                                 filetypes=
-                                                 [
-                                                     ('text files', ('.txt'))
-                                                 ])
-        if save_location is None or not save_location or save_location == "":
-            return
+        try:
+            save_location = filedialog.asksaveasfile(mode='w', initialfile="cipher", defaultextension=".txt",
+                                                     filetypes=
+                                                     [
+                                                         ('text files', ('.txt'))
+                                                     ])
+            if save_location is None or not save_location or save_location == "":
+                return
 
-        message = self.input_text.get("1.0", END)
-        save_location.write(message)
-        save_location.close()
+            message = self.input_text.get("1.0", END)
+            save_location.write(message)
+            save_location.close()
+        except Exception as e:
+            raise Exception("Soubor se nepovedlo uložit:  " + str(e))
 
     def vector_callback(self):
         #callback funkce pro nastavení vektoru z GUI
@@ -156,6 +197,8 @@ class MainWindow:
             self.key_len = 32
 
     def clear_btn_callback(self):
+        self.input_text.config(state=NORMAL)
+        self.errorLabel.config(text="")
         self.input_text.delete(1.0, "end")
         self.key_field.delete(0, END)
 
